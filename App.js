206f2fd18cb73c15fdfbe2bfb7b8efd1990bba4d
root <root@420-512-1-mawhoubyah.10.10.0.5>
2024-09-12 18:08:19 +0000

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useRef, useState } from 'react';
+import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+//import { withDelay } from 'react-native-reanimated';
+
+
 
 export default function App() {
+
+  const time = useRef(new Date())
+  const [numberOfClicks, setNumberOfClicks] = useState(0);
+  const [textSeconds, setTextSeconds] = useState("Appuyer 10 fois le plus vite possible");
+
+  const handleNumberClicks = () => {
+    
+    if(numberOfClicks == 0){
+      setNumberOfClicks(1);
+      time.current = (new Date());
+    }
+
+    else if(numberOfClicks == 9){
+      console.log("temps: " + (((new Date()) - time.current) / 1000))
+      setNumberOfClicks(0);
+
+      setTextSeconds("Vous avez appuyé 10 fois en " + ((new Date() - time.current) / 1000) + " secondes")
+    }
+
+    else{
+      setNumberOfClicks(numberOfClicks + 1);
+    }
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.numberClicks}>{numberOfClicks}</Text>
+
+      <View style={styles.informationContainer}>
+        <Text style={styles.informationText}>{textSeconds}</Text>
+        <TouchableOpacity style={styles.clickButton} onPress={handleNumberClicks}>
+          <Text style={styles.clickButtonText}>Appuyez sur moi</Text>
+        </TouchableOpacity>
+      </View>
+      
     </View>
   );
 }
@@ -13,8 +49,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f0f0f0',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center', //
   },
+
+  numberClicks: {
+    fontSize: 60,
+  },
+
+  informationContainer: {
+    marginTop: 150
+  },
+
+  informationText: {
+    
+  },
+
+  clickButton: {
+    paddingVertical: 30,
+    backgroundColor: "#ff4d4d",
+    borderRadius: 10,
+    marginTop: 20,
+    width: Dimensions.get("window").width / 2,
+    alignSelf: 'center'
+  },
+
+  clickButtonText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: "#fff",
+    textAlign:"center",
+  }
 });
